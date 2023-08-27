49db1620155a6fed84247f9919153c5ef5bdc4af
Daniela Liendo <daniela@ualabee.com>
2023-08-27 19:19:43 +0200

    Eslint config was added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import {ReactNode} from "react";
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
