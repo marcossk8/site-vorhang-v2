b24671844ef67d34ed0dd22c3a7366f05fda1f93
Daniela Liendo <daniela@ualabee.com>
2023-09-26 20:43:47 +0200

    default html language changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
   )
